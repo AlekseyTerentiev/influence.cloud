8e38b5c92a6a30c388bf276915338f90c11fb9c0
Alexey <alexey.terentiev.mail@gmail.com>
2020-07-21 16:55:23 +0300

    <Loading/> fixed

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -5,13 +5,20 @@ export function Loading() {
   return (
     <div
       style={{
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
+        position: 'relative',
+        width: '100%',
+        height: 80,
       }}
     >
-      <img src={loadingSvg} />
+      <img
+        src={loadingSvg}
+        style={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+        }}
+      />
     </div>
   );
 }
