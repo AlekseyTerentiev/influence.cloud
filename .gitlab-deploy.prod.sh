21d812328c0f53dd797ab715dba7e2dbe45a1840
Navruz Jandullaev <navruzjn@Navruzs-MacBook-Air.local>
2020-08-05 20:18:27 +0500

    fixed gitlab prod deploy

--- .gitlab-deploy.prod.sh
+++ .gitlab-deploy.prod.sh
@@ -11,5 +11,5 @@ array=(${string//,/ })
 # Careful with the ; https://stackoverflow.com/a/20666248/1057052
 for i in "${!array[@]}"; do
   echo "Deploy project on server ${array[i]}"
-  ssh ubuntu@${array[i]} "cd ./influence_cloud/vr && git stash  && git checkout $CI_BUILD_REF_NAME && git stash && git pull origin master && sudo yarn install && yarn start"
+  ssh ubuntu@${array[i]} "cd ./influence_cloud_frontend/vr && git stash  && git checkout $CI_BUILD_REF_NAME && git stash && git pull origin master && sudo yarn install && npm run build && pm2 delete influence_cloud_frontend && pm2 start node_modules/react-scripts/scripts/start.js --name 'influence_cloud_frontend'"
 done
