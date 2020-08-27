ab424906e4d03fea6d358dad481c3be2f18524cd
Alexey <alexey.terentiev.mail@gmail.com>
2020-08-27 15:18:03 +0300

    Localization fix

--- src/i18n/en.ts
+++ src/i18n/en.ts
@@ -2,6 +2,12 @@ export default {
   translation: {
     acc_rating: 'Rating',
 
+    // Account types
+    autoMoto: 'auto / moto',
+    filmDirector: 'film director',
+    healthBeauty: 'health / beauty',
+    publicFigure: 'public figure',
+
     // Task status
     inProgress: 'in progress',
 
