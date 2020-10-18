0fc711d753b83ac9074c05e99e68af979e18f31d
Alexey <alexey.terentiev.mail@gmail.com>
2020-10-19 00:47:03 +0300

    Task fragment added

---
+++ src/gql/types/TaskFragment.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL fragment: TaskFragment
+// ====================================================
+
+export interface TaskFragment {
+  __typename: "InstagramCommentTask" | "InstagramStoryTask";
+  waitingAccountTasks: number;
+}
